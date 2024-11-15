7866ba5b0c88d9e2367d5ed17ff57b8bb53d82db
Mitsokiu <thuannisan@gmail.com>
2024-11-15 20:44:12 +0700

    themusermgn

--- asset/js/admin.js
+++ asset/js/admin.js
@@ -5,6 +5,10 @@ const closeBtn = document.getElementById('xadd');
 const fixProductBtn = document.getElementById('fix-icon');
 const fixForm = document.querySelector('.pageadmin-content-fix');
 const closefixForm= document.getElementById('xfix');
+const rvProduct = document.getElementById('trash-icon');
+const rvcancel = document.getElementById('trash-cancel');
+const rvok = document.getElementById('trash-ok');
+const rvForm = document.querySelector('.pageadmin-trashbox');
 addProductBtn.addEventListener('click', function () {
     popupForm.style.display = 'flex';
 });
@@ -19,6 +23,17 @@ closeBtn.addEventListener('click', function () {
 
 closefixForm.addEventListener('click', function () {
     fixForm.style.display = 'none'; 
+});
+
+rvProduct.addEventListener('click', function(){
+    rvForm.style.display='flex';
+});
+rvok.addEventListener('click',function(){
+    rvForm.style.display='none';
+});
+
+rvcancel.addEventListener('click',function(){
+    rvForm.style.display='none';
 });
 
 
